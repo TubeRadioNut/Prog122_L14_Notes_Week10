d36060997109b2f03062d4743492c38895f3a18a
rt65s <rt65s@SCHOOL_LAPTOP>
2024-06-03 19:54:09 -0700

    Add project files.

---
+++ Art.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog122_L14_Notes_Week10
+{
+    public class Art
+    {
+        //Enumerables
+        // Keyword: enum
+        //Fields
+        public enum STYLE { Abstract, Impressionism, Cubism }
+        STYLE _style;
+        string _name;
+
+        
+
+        //Constructor
+        public Art(string name, STYLE style)
+        {
+            _name = name;
+            _style = style;
+
+
+        }
+        //Properties
+
+        public STYLE Style { get => _style; set => _style = value; }
+        public string Name { get => _name; set => _name = value; }
+
+        //Method
+
+        public override string ToString()
+        {
+            return $"{_name} - {_style}";
+        }
+    }//end class
+}//end namespace
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Prog122_L14_Notes_Week10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillComboBox();
+            string imgPath = @"C:\Users\rt65s\Documents\Software II\Week 10\Prog122_L14_Notes_Week10\Images\Grade.jpg";
+            imgDisplay.Source = ConvertToImage(imgPath);
+
+
+        }
+
+        public BitmapImage ConvertToImage(string filePath)
+        {
+            string imgPath = filePath;
+            //Uniform Resource Identifier
+            Uri converPath = new Uri(imgPath);
+            BitmapImage bitmap = new BitmapImage(converPath);
+            return bitmap;
+        }
+
+        public void FillComboBox()
+        {
+            Art nighthawks = new Art("Nighthawks", Art.STYLE.Impressionism);
+
+            runDisplay.Text = nighthawks.ToString();
+            //EnumComboBox.ItemsSource = Enum.GetValues(typeof(MyEnum)).Cast<MyEnum>().ToList();
+            //controlName.ItemsSource = Enum.GetValues(typeof(enumName)).Cast<enumName>().ToList
+            cmbStyles.ItemsSource = Enum.GetValues<Art.STYLE>().Cast<Art.STYLE>().ToList();
+
+        }
+
+        private void btnLoadImage_Click(object sender, RoutedEventArgs e)
+        {
+            //step 1 Open file dialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //Filter
+            //What displays in drop down box
+            string displayFilter = "Image files (*.png;*jpeg;*.jpg)";
+            //What is used to filter results in file explorer
+            string filterBy = "*.png;*.jpeg;*.jpg";
+            //Full stringf needed
+            //MUST HAVE PIPE BETWEEN DISPLAY AND FILTER
+            string fullFilter = $"{displayFilter}|{filterBy}";
+            //Pass to filter
+            openFileDialog.Filter = fullFilter;
+            //openFileDialog.Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg";
+
+            //opens the dialog and returns true if an image is selected
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string returnedFilePath = openFileDialog.FileName;
+                imgDisplay.Source = ConvertToImage(returnedFilePath);
+            }
+        }
+    }
+}
